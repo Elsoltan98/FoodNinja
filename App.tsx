a64cb907e21189f9c9da96b14bda7e2331a17f05
MACBOOK <ben10a2992@gmail.com>
2024-04-20 16:56:43 +0200

    feat: add splash screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
 import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
 import React from 'react';
 import Colors from './src/config/colors';
-import CustomIcon from './src/components/Icon';
-import { fontScale } from './src/config/scale';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View>
-        <Text>Start codeing Coffee App</Text>
-        <CustomIcon
-          type="AntDesign"
-          name="minuscircle"
-          size={fontScale(30)}
-          color={Colors.dark.black}
-        />
+        <Text>Food Ninga App</Text>
       </View>
     </SafeAreaView>
   );
--- ios/foodNinjaTests/foodNinjaTests.m
+++ ios/foodNinjaTests/foodNinjaTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface coffeeTests : XCTestCase
+@interface foodNinjaTests : XCTestCase
 
 @end
 
-@implementation coffeeTests
+@implementation foodNinjaTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {

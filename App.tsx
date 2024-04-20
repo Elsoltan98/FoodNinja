2512f7121bcb0c1c68cc495f45d99822d34bd749
MACBOOK <ben10a2992@gmail.com>
2024-04-20 23:32:13 +0200

    feat: add navigation to app

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
 import React, { useEffect } from 'react';
 import Colors from './src/config/colors';
 import SplashScreen from 'react-native-splash-screen';
+import AppNav from './src/navigation';
 
 const App = () => {
   useEffect(() => {
@@ -16,9 +17,7 @@ const App = () => {
   }, []);
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <Text>Food Ninga App</Text>
-      </View>
+      <AppNav />
     </SafeAreaView>
   );
 };
---
+++ src/navigation/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Intro from '../screens/intro';
+const Stack = createNativeStackNavigator();
+const AppNav = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Intro" component={Intro} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNav;

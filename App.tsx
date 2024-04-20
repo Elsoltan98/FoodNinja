0c7ab7c57c3512ce4af3061bf313fa261aedc9bd
MACBOOK <ben10a2992@gmail.com>
2024-04-20 18:10:55 +0200

    feat: add splash screen to ios version

--- App.tsx
+++ App.tsx
@@ -1,8 +1,19 @@
 import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Colors from './src/config/colors';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (SplashScreen) {
+        SplashScreen.hide();
+      }
+    }, 2000);
+
+    // Clear the timer on component unmount
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <SafeAreaView style={styles.container}>
       <View>

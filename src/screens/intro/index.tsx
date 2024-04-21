0756da3e1d7097481b9cc70a9a954cda19648cd9
MACBOOK <ben10a2992@gmail.com>
2024-04-21 03:10:16 +0200

    feat: finish Onboarding screens

--- src/screens/intro/index.tsx
+++ src/screens/intro/index.tsx
@@ -1,32 +1,58 @@
 import { View, Text, StyleSheet } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import { SvgXml } from 'react-native-svg';
 import { Intro1 } from '../../assets/intro1';
 import { fontScale, sHeight, sWidth, vScale } from '../../config/scale';
 import Colors from '../../config/colors';
 import AppText from '../../components/AppText';
 import CustomBtn from '../../components/CustomBtn';
+import { Intro2 } from '../../assets/intro2';
 
 const Intro = () => {
+  const [nextIntro, setNextIntro] = useState<Number>(0);
   return (
     <View style={styles.container}>
-      <SvgXml xml={Intro1} width={sWidth * 0.9} height={sHeight * 0.5} />
-      <View style={styles.findTextContainer}>
-        <AppText
-          text={`Find your Comfort \n Food here`}
-          color={Colors.light.textPrimary}
-          fontWeight="bold"
-          size={fontScale(22)}
-          style={{ textAlign: 'center' }}
-        />
-        <AppText
-          text={`Here You Can find a chef or dish for every \n taste and color. Enjoy!`}
-          style={{ textAlign: 'center', marginTop: vScale(30) }}
-          color={Colors.light.black}
-          size={fontScale(12)}
-        />
-      </View>
-      <CustomBtn title="Next" />
+      {nextIntro === 0 ? (
+        <>
+          <SvgXml xml={Intro1} width={sWidth * 0.9} height={sHeight * 0.5} />
+          <View style={styles.findTextContainer}>
+            <AppText
+              text={`Find your Comfort \n Food here`}
+              color={Colors.light.textPrimary}
+              fontWeight="bold"
+              size={fontScale(22)}
+              style={{ textAlign: 'center' }}
+            />
+            <AppText
+              text={`Here You Can find a chef or dish for every \n taste and color. Enjoy!`}
+              style={{ textAlign: 'center', marginTop: vScale(30) }}
+              color={Colors.light.black}
+              size={fontScale(12)}
+            />
+          </View>
+          <CustomBtn title="Next" onPress={() => setNextIntro(1)} />
+        </>
+      ) : (
+        <>
+          <SvgXml xml={Intro2} width={sWidth * 0.8} height={sHeight * 0.5} />
+          <View style={styles.findTextContainer}>
+            <AppText
+              text={`Food Ninja is Where Your \nComfort Food Lives`}
+              color={Colors.light.textPrimary}
+              fontWeight="bold"
+              size={fontScale(22)}
+              style={{ textAlign: 'center' }}
+            />
+            <AppText
+              text={`Enjoy a fast and smooth food delivery at \nyour doorstep`}
+              style={{ textAlign: 'center', marginTop: vScale(30) }}
+              color={Colors.light.black}
+              size={fontScale(12)}
+            />
+          </View>
+          <CustomBtn title="Finish" />
+        </>
+      )}
     </View>
   );
 };

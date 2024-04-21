c3ab0543d5e3fd411bfbbbd3a00924c7143a71e3
MACBOOK <ben10a2992@gmail.com>
2024-04-21 21:33:00 +0200

    Add buttons to login screen

--- src/components/CustomInput/index.tsx
+++ src/components/CustomInput/index.tsx
@@ -1,6 +1,13 @@
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  NativeSyntheticEvent,
+  TextInputFocusEventData,
+} from 'react-native';
 import React, { FC, ReactElement } from 'react';
-import { fontScale, sHeight, scale, vScale } from '../../config/scale';
+import { fontScale, sHeight, sWidth, scale, vScale } from '../../config/scale';
 import Colors from '../../config/colors';
 
 interface CustomInputProps {
@@ -8,6 +15,13 @@ interface CustomInputProps {
   inputPlaceHolder: string;
   leftIcon?: ReactElement;
   rightIcon?: ReactElement;
+  value?: string;
+  onChangeText?: (text: string) => void;
+  onBlur?:
+    | ((e: NativeSyntheticEvent<TextInputFocusEventData>) => void)
+    | undefined;
+  isPassword?: boolean;
+  show?: boolean;
 }
 
 const CustomInput: FC<CustomInputProps> = ({
@@ -15,6 +29,11 @@ const CustomInput: FC<CustomInputProps> = ({
   inputPlaceHolder,
   leftIcon,
   rightIcon,
+  value,
+  onChangeText,
+  onBlur,
+  isPassword,
+  show,
 }) => {
   return (
     <View>
@@ -22,14 +41,25 @@ const CustomInput: FC<CustomInputProps> = ({
         <View style={[styles.inputContainer, styles.iconsContainer]}>
           <View>
             {leftIcon && leftIcon}
-            <TextInput placeholder={inputPlaceHolder} />
+            <TextInput
+              placeholder={inputPlaceHolder}
+              value={value}
+              onChangeText={onChangeText}
+              onBlur={onBlur}
+              secureTextEntry={isPassword ? show : false}
+              style={styles.inputWidth}
+            />
           </View>
           {rightIcon && rightIcon}
         </View>
       ) : (
         <TextInput
-          style={styles.inputContainer}
+          style={[styles.inputContainer]}
           placeholder={inputPlaceHolder}
+          value={value}
+          onChangeText={onChangeText}
+          onBlur={onBlur}
+          secureTextEntry={isPassword ? show : false}
         />
       )}
     </View>
@@ -38,7 +68,7 @@ const CustomInput: FC<CustomInputProps> = ({
 
 const styles = StyleSheet.create({
   inputContainer: {
-    height: sHeight * 0.06,
+    height: sHeight * 0.065,
     backgroundColor: Colors.light.white,
     borderRadius: scale(15),
     borderWidth: scale(1),
@@ -47,7 +77,7 @@ const styles = StyleSheet.create({
     elevation: 20,
     shadowColor: Colors.light.shadow,
     shadowOffset: { width: 2, height: 5 },
-    shadowOpacity: 0.4,
+    shadowOpacity: 0.3,
     shadowRadius: 15,
     paddingHorizontal: scale(25),
     fontSize: fontScale(16),
@@ -56,6 +86,9 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     flexDirection: 'row',
+  },
+  inputWidth: {
+    width: sWidth * 0.63,
   },
 });
 
--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,5 +1,5 @@
 import { View, Text, ImageBackground, StyleSheet } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import { fontScale, sHeight, sWidth, scale, vScale } from '../../config/scale';
 import { SvgXml } from 'react-native-svg';
 import { Logo } from '../../assets/Logo';
@@ -7,8 +7,11 @@ import AppText from '../../components/AppText';
 import Colors from '../../config/colors';
 import CustomInput from '../../components/CustomInput';
 import CustomIcon from '../../components/Icon';
+import { Formik } from 'formik';
+import CustomBtn from '../../components/CustomBtn';
 
 const Login = () => {
+  const [show, setShow] = useState<boolean>(false);
   return (
     <View style={{ flex: 1 }}>
       <ImageBackground
@@ -23,20 +26,65 @@ const Login = () => {
             fontWeight="bold"
           />
         </View>
-        <View style={styles.formContainer}>
-          <CustomInput inputPlaceHolder="Email" />
-          <CustomInput
-            withIcons
-            inputPlaceHolder="Password"
-            rightIcon={
-              <CustomIcon
-                type="FontAwesome6"
-                name="eye"
-                color={Colors.light.gridPrimary}
-                size={fontScale(20)}
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          onSubmit={values => console.log(values)}>
+          {props => (
+            <View style={styles.formContainer}>
+              <CustomInput
+                inputPlaceHolder="Email"
+                value={props.values.email}
+                onChangeText={props.handleChange('email')}
+                onBlur={props.handleBlur('email')}
               />
-            }
-          />
+              <CustomInput
+                withIcons
+                inputPlaceHolder="Password"
+                isPassword
+                show={show}
+                value={props.values.password}
+                onChangeText={props.handleChange('password')}
+                onBlur={props.handleBlur('password')}
+                rightIcon={
+                  !show ? (
+                    <CustomIcon
+                      type="FontAwesome6"
+                      name="eye-slash"
+                      color={Colors.light.gridPrimary}
+                      size={fontScale(20)}
+                      onPress={() => setShow(!show)}
+                    />
+                  ) : (
+                    <CustomIcon
+                      type="FontAwesome6"
+                      name="eye"
+                      color={Colors.light.gridPrimary}
+                      size={fontScale(20)}
+                      onPress={() => setShow(!show)}
+                    />
+                  )
+                }
+              />
+            </View>
+          )}
+        </Formik>
+        <View
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: vScale(10),
+            marginBottom: vScale(20),
+          }}>
+          <AppText text="Or Continue With" fontWeight="bold" />
+          <View style={styles.socialContainer}>
+            <CustomBtn title="Facebook" />
+            <CustomBtn title="Google" />
+          </View>
+          <AppText text="Forgot Your Password?" fontWeight="bold" />
+        </View>
+        <View>
+          <CustomBtn title="Login" />
+          <AppText text="Don’t have an account? Sign up" />
         </View>
       </ImageBackground>
     </View>
@@ -54,6 +102,12 @@ const styles = StyleSheet.create({
   },
   formContainer: {
     marginTop: vScale(50),
+  },
+  socialContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginVertical: vScale(20),
   },
 });
 

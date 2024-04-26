9d41d00ba82d8b88400e40a62431f12f6f5b4933
MACBOOK <ben10a2992@gmail.com>
2024-04-26 23:16:45 +0300

    Add Back button in signup process screen

--- src/screens/SignUpProcess/index.tsx
+++ src/screens/SignUpProcess/index.tsx
@@ -1,10 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import CustomIcon from '@components/Icon'
+import { fontScale } from '@config/scale'
 
 const SignupProcess = () => {
   return (
     <View style={styles.container}>
-      <Text>SignupProcess</Text>
+      <TouchableOpacity style={styles.backContainer}>
+        <CustomIcon type="Entypo" name="chevron-left" color='#DA6317' size={fontScale(22)} />
+      </TouchableOpacity>
     </View>
   )
 }
@@ -12,6 +16,15 @@ const SignupProcess = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1
+    },
+    backContainer: {
+      backgroundColor: '#F9A84D',
+      width: 45,
+      height: 45,
+      padding: 10,
+      borderRadius: 15,
+      justifyContent: 'center',
+      alignItems: 'center'
     }
 })
 export default SignupProcess

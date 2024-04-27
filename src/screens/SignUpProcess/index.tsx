1c1cb554e65c80b4ead411a30b37af02b78bcbb8
MACBOOK <ben10a2992@gmail.com>
2024-04-28 02:24:51 +0300

    Fix icon to back

--- src/screens/SignUpProcess/index.tsx
+++ src/screens/SignUpProcess/index.tsx
@@ -1,6 +1,7 @@
 import { ImageBackground, StyleSheet, TouchableOpacity, View } from 'react-native'
 import React from 'react'
-import CustomIcon from '@components/Icon'
+import { SvgXml } from 'react-native-svg'
+import { BackBg } from '@assets/BackBg'
 
 
 const SignupProcess = () => {
@@ -10,7 +11,7 @@ const SignupProcess = () => {
         source={require('./../../assets/bg.png')}
         style={styles.container}>
       <TouchableOpacity style={styles.backContainer}>
-        <CustomIcon type="Entypo" name='chevron-left' size={20} color='#DA6317' />
+        <SvgXml xml={BackBg}  />
       </TouchableOpacity>
       </ImageBackground>
     </View>

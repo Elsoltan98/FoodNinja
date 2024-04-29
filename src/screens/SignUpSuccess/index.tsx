dd56f3ea911dab25cec8bbc81f7a9eea35c3c1bc
MACBOOK <ben10a2992@gmail.com>
2024-04-29 03:10:36 +0300

    Finish SignUp Success screen

--- src/screens/SignUpSuccess/index.tsx
+++ src/screens/SignUpSuccess/index.tsx
@@ -1,10 +1,38 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, View } from "react-native";
 import React from "react";
+import { SvgXml } from "react-native-svg";
+import { Success } from "@assets/Success";
+import AppText from "@components/AppText";
+import Colors from "@config/colors";
+import { fontScale, vScale } from "@config/scale";
+import CustomBtn from "@components/CustomBtn";
 
 const SignUpSuccess = () => {
   return (
     <View style={styles.container}>
-      <Text>SignUpSuccess</Text>
+      <ImageBackground
+        source={require("@assets/bg.png")}
+        style={styles.contentContainer}
+      >
+        <View style={styles.congratsContainer}>
+          <SvgXml xml={Success} />
+          <AppText
+            text="Congrats!"
+            color={Colors.light.gridSecondary}
+            style={styles.congratsText}
+            fontWeight="bold"
+            size={fontScale(30)}
+          />
+          <AppText
+            text="Your Profile Is Ready To Use"
+            color={Colors.light.textPrimary}
+            fontWeight="bold"
+            size={fontScale(23)}
+          />
+        </View>
+
+        <CustomBtn title="Try Order" style={styles.tryBtn} />
+      </ImageBackground>
     </View>
   );
 };
@@ -12,6 +40,26 @@ const SignUpSuccess = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  congratsText: {
+    marginTop: vScale(33),
+    marginBottom: vScale(16),
+  },
+  congratsContainer: {
+    flex: 4,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  tryBtn: {
+    flex: 1,
+    alignItems: "flex-end",
+    justifyContent: "flex-end",
+    top: -40,
   },
 });
 export default SignUpSuccess;
